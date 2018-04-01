1109a15381a7d67e36c400cc1b36fe5dfe778d35
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-01 16:44:29 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,7 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
-  def full_name
+  def full_name(actor = self)
     self.first_name self.last_name
   end
 end
