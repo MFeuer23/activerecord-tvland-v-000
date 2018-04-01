02d214cd9c9fc7b637709c8df72210fa2fbab119
Mallory Feuer <mallory.feuer@gmail.com>
2018-04-01 17:02:03 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
   def list_roles
     characters.collect do |character|
       shows.collect do |show|
-        "#{character} - #{show}"
+        "#{character.name} - #{show.name}"
       end
     end
   end
